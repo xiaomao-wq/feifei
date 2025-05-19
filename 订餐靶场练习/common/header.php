5279a6412c8b845c33358f688ed606d1e2e6490b
xiaomao-wq <3457969093@qq.com>
2025-05-19 13:10:32 +0800

    Add files via upload

---
+++ 订餐靶场练习/common/header.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+unset($_SESSION['memberName']);
+unset($_SESSION['memberId']);
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8"/>
+    <title>网上订餐-登录</title>
+    <meta name="keywords" content="DeathGhost,DeathGhost.cn,web前端设,移动WebApp开发"/>
+    <meta name="description" content="DeathGhost.cn::H5 WEB前端设计开发!"/>
+    <meta name="author" content="DeathGhost"/>
+    <link href="../static/style/style.css" rel="stylesheet" type="text/css"/>
+    <script type="text/javascript" src="../static/js/public.js"></script>
+    <script type="text/javascript" src="../static/js/jquery.js"></script>
+    <script type="text/javascript" src="../static/js/jqpublic.js"></script>
+</head>
